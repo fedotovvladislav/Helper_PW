972befb371347379af42d6d36095b774dd5a9e65
Vladislav Fedotov <fleetvord@gmail.com>
2024-02-16 18:53:14 +0300

    update oberon

--- handlers/driver.py
+++ handlers/driver.py
@@ -12,7 +12,7 @@ def get_driver():
     options.add_argument('--no-sandbox')
     options.add_argument('--start-maximized')
     options.add_experimental_option("prefs", prefs)
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     driver = uc.Chrome(use_subprocess=True, options=options)
     wait = WebDriverWait(driver, 10)
 
